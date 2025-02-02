5d98e2df40caf21266957ec322e4b34eff7001fb
chronometer <jlipponen@gmail.com>
2025-02-02 15:47:21 +0200

    chore: Remove unnecessary files

--- test_app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title('Connection Test')
-st.success('If you see this, Streamlit is working!')
-st.write('Server time: ' + st.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
